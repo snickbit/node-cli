f37f3a988cfc503b6a17b466cae63f888da632c0
Snickbit <snickbit@users.noreply.github.com>
2022-06-03 20:42:34 -0400

    fix: remove useless generic

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import {ParsedArgs} from './definitions'
 /**
  * Simple Node.js CLI framework for creating command line applications.
  */
-export function cli<T extends ParsedArgs = any>(name?: string): Cli
+export function cli(name?: string): Cli
 export function cli<T extends ParsedArgs = any>(args?: T): Cli
 export function cli<T extends ParsedArgs = any>(name?: string, args?: T): Cli
 export function cli<T extends ParsedArgs = any>(nameOrArgs?: T | string, optionalArgs?: T): Cli<T> {
