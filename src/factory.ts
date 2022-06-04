a25ee9f442898d5accee5ade036579db415fa4c2
Snickbit <snickbit@users.noreply.github.com>
2022-06-04 15:36:52 -0400

    fix: adjust exports

---
+++ src/factory.ts
@@ -0,0 +1,12 @@
+import {Cli} from './Cli'
+import {ParsedArgs} from './definitions'
+
+/**
+ * Simple Node.js CLI framework for creating command line applications.
+ */
+export function cli(name?: string): Cli
+export function cli<T extends ParsedArgs = any>(args?: T): Cli
+export function cli<T extends ParsedArgs = any>(name?: string, args?: T): Cli
+export function cli<T extends ParsedArgs = any>(nameOrArgs?: T | string, optionalArgs?: T): Cli<T> {
+	return new Cli<T>(nameOrArgs as any, optionalArgs)
+}
